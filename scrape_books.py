902e434385d4c6410678799048bc29f8253af256
KashishJain27 <k2005.jain@gmail.com>
2026-01-22 21:15:42 +0530

    Add files via upload

---
+++ scrape_books.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "http://books.toscrape.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+books = soup.find_all("article", class_="product_pod")
+data = []
+
+for book in books:
+    title = book.h3.a["title"]
+    price = book.find("p", class_="price_color").text.replace("Â", "")
+    availability = book.find("p", class_="instock availability").text.strip()
+    
+    data.append({
+        "Title": title,
+        "Price": price,
+        "Availability": availability
+    })
+
+df = pd.DataFrame(data)
+df.to_csv("books.csv", index=False, encoding="utf-8-sig")
+
+print("Scraping complete! Check books.csv in this folder.")
